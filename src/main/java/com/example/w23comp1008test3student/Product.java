031628e125e529494c9e7ec7b6df2932bb583147
shoaib imran <200528971@student.georgianc.on.ca>
2023-04-20 16:12:01 -0400

    question 4 complete

--- src/main/java/com/example/w23comp1008test3student/Product.java
+++ src/main/java/com/example/w23comp1008test3student/Product.java
@@ -1,7 +1,6 @@
 package com.example.w23comp1008test3student;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Product {
@@ -11,14 +10,21 @@ public class Product {
     private ArrayList<Review> reviews;
 
     public Product(String name, double price, String category) {
+        setName(name);
+        setPrice(price);
+        setCategory(category);
+        reviews = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-
+    public void setName(String name) { // i got this name trim function off stackoverflow
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name can not be null or empty");
+        }
+        this.name = name;
     }
 
     public double getPrice() {
@@ -26,7 +32,10 @@ public class Product {
     }
 
     public void setPrice(double price) {
-
+        if (price <= 0) {
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+        this.price = price;
     }
 
     public String getCategory() {
@@ -34,24 +43,36 @@ public class Product {
     }
 
     public void setCategory(String category) {
+        if (category == null || category.trim().isEmpty()) { // this trim function is also from stackoverflow but i made it for a catagory  instead of name
+            throw new IllegalArgumentException("Category cannot be null or empty");
+        }
+        this.category = category;
     }
 
-    public ArrayList<Review> getReviews() {
+    public List<Review> getReviews() {
         return reviews;
     }
 
-    public void addReview(Review review)
-    {
-
+    public void addReview(Review review) {
+        if (review == null) {
+            throw new IllegalArgumentException("Review cannot be empty");
+        }
+        reviews.add(review);
     }
 
-    public String getRating()
-    {
-       return null;
+    public String getRating() {
+        if (reviews.isEmpty()) {
+            return "Nothing";
+        }
+        double sum = 0;
+        for (Review review : reviews) {
+            sum += review.getStarRating();
+        }
+        double average = sum / reviews.size();
+        return String.format("%.1f", average); // i had to look this up to get the average using floats
     }
 
-    public String toString()
-    {
-        return null;
+    public String toString() {
+        return String.format("%s (%s) - $%.2f, rating: %s", name, category, price, getRating());
     }
 }
